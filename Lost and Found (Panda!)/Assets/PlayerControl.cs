00b05d768e20ba2d169200b140ed4f0f1c692970
Matthew Borres <matt931998gmail.com>
2019-02-22 00:48:36 +0800

    Ok na controls ng player. Icacalibrate ko na lang yung camera.

--- Lost and Found (Panda!)/Assets/PlayerControl.cs
+++ Lost and Found (Panda!)/Assets/PlayerControl.cs
@@ -5,27 +5,43 @@ using UnityEngine;
 public class PlayerControl : MonoBehaviour
 {
 	public Rigidbody2D Player_rb;
-	public Vector2 Walk_Speed;
-	public Vector2 Jump_Speed;
 
-    void FixedUpdate()
+    public float Walk_Speed;
+    public float Jump_Speed;
+
+    private bool On_Ground;
+
+    void Update()
     {
 		SpeedBasedMovement();
     }
 
 	void SpeedBasedMovement(){
 
-		if (Input.GetKeyDown(KeyCode.D))
+		if (Input.GetKey(KeyCode.D) && On_Ground == true)
 		{
-			Player_rb.velocity = Walk_Speed;
+			Player_rb.velocity = new Vector2 (Walk_Speed, Player_rb.velocity.y);
 		}
-		if (Input.GetKeyDown(KeyCode.A))
+		if (Input.GetKey(KeyCode.A) && On_Ground == true)
 		{
-			Player_rb.velocity = -Walk_Speed;
-		}
-		if (Input.GetKeyDown(KeyCode.W))
+            Player_rb.velocity = new Vector2(-Walk_Speed, Player_rb.velocity.y);
+        }
+        if (Input.GetKeyDown(KeyCode.W) && On_Ground == true)
 		{
-			Player_rb.velocity = Jump_Speed;
-		}
+            Player_rb.velocity = new Vector2(Player_rb.velocity.x, Jump_Speed);
+        }
 	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if ((collision.collider.tag == "Ground"))
+        {
+            On_Ground = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        On_Ground = false;
+    }
 }
