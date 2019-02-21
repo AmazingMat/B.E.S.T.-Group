123fe66ecd8c0505ed2f5b911fcb88018e714339
Matthew Borres <matt931998gmail.com>
2019-02-21 10:10:50 +0800

    test

---
+++ Lost and Found (Panda!)/Assets/PlayerControl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+	public Rigidbody2D Player_rb;
+	public float Walk_Speed;
+	public float Jump_Speed;
+
+    void FixedUpdate()
+    {
+		SpeedBasedMovement();
+    }
+
+	void SpeedBasedMovement(){
+
+		if (Input.GetKeyDown(KeyCode.D))
+		{
+			Player_rb.velocity.x = 3;
+		}
+		if (Input.GetKeyDown(KeyCode.A))
+		{
+			Player_rb.velocity.x = -3;
+		}
+		if (Input.GetKeyDown(KeyDown.W))
+		{
+			Player_rb.velocity.y = 3;
+		}
+	}
+}
